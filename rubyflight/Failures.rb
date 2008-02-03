cd145892da9958258ea394c2e594cbaee8efdd38
v01d <phreakuencies@gmail.com>
2008-02-03 16:40:58 +0000

    - add this

---
+++ rubyflight/Failures.rb
@@ -0,0 +1,19 @@
+module RubyFlight
+  # NOT YET FUNCTIONAL
+  class Failures
+    def initialize(failures = {})
+      @failures = failures
+    end
+    
+    def from_xml(elem)
+      
+    end
+
+    def set_probability(type)
+    end
+    
+    def process
+            
+    end
+  end
+end
